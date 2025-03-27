7832e93b8958b6f2f6a6f1342d920a7522e2fb62
gabrielbarbozajs <gabrielbarboza.js@gmail.com>
2025-03-27 09:55:08 -0300

    site teste com js

--- Site-Teste-main/script.js
+++ Site-Teste-main/script.js
@@ -1,4 +1,18 @@
-const caixaverde = document.getElementById("caixa verde");
-function mudarParaPreto (caixaverde) {
-    caixaverde.style.backgroundColor = "black";}
-    caixaverde.addEventListener("click", mudarParaPreto);
+// Seleciona todas as divs com a classe "caixa"
+let divs = document.querySelectorAll(".caixa");
+
+// Adiciona um evento de clique para cada div
+divs.forEach(div => {
+    
+    let corInicial = div.style.backgroundColor;
+
+    div.addEventListener("click", function() {
+        // Se a cor atual for preta, volta para a cor inicial
+        if (div.style.backgroundColor === "black") {
+            div.style.backgroundColor = corInicial;
+        } else {
+            // Se não for preta, muda para a cor preta
+            div.style.backgroundColor = "black";
+        }
+    });
+});
